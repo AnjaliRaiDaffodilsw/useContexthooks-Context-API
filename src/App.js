65e4568f24c1aabc0ad7f3f454a975767eeb531f
AnjaliRaiDaffodilsw <anjali.rai@unthinkable.co>
2021-04-16 12:08:43 +0530

    useContext Hooks and context API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
+import { createContext } from 'react';
 import './App.css';
 
+import ComA from './CompA';
+
+const FirstName = createContext();
+const LastName = createContext();
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FirstName.Provider value={"Vidhya"}>
+        <LastName.Provider value={"Rai"}>
+          <ComA />
+        </LastName.Provider>
+      </FirstName.Provider>
+    </>
   );
 }
 
 export default App;
+export { FirstName , LastName };
